a9ac5bb08e7da61a4186e17eaa5b139997663bfa
anthony2018317 <kantvoltairerosseau@gmail.com>
2019-01-20 03:37:33 -0800

    Update parkingService.js

--- parkingService.js
+++ parkingService.js
@@ -4,7 +4,7 @@ var PermitAuthRequestCharacteristic = require('./permitAuth.js')
 
 function ParkingService(parkingSpot) {
 	bleno.PrimaryService.call(this, {
-		uuid: '13333333333333333333333333333337',
+		uuid: '8556f0aa-38ec-4f2c-a499-021c5c8c4a49',
 		characteristics: [
 			new PermitAuthRequestCharacteristic(parkingSpot)
 		]
