5904f8eedfb9e2b9e35f0934f6278fa167970c79
Ronak Shah <ronakshahultimate@gmail.com>
2019-01-20 02:35:40 -0800

    qc

--- permitAuth.js
+++ permitAuth.js
@@ -6,6 +6,7 @@ const verifyId = require('./verifyId.js')
 // let LED = new Gpio(4, 'out')
 
 function PermitAuthRequestCharacteristic(parkingSpot) {
+	this.parkingSpot = parkingSpot
 	bleno.Characteristic.call(this, {
 		uuid: '13333333333333333333333333330001',
 		properties: [
@@ -15,26 +16,26 @@ function PermitAuthRequestCharacteristic(parkingSpot) {
 }
 
 PermitAuthRequestCharacteristic.prototype.onWriteRequest = function(data, offset, withoutResponse, callback) {
-			if (offset) {
-				callback(this.RESULT_ATTR_NOT_LONG);
-			} else if (data.length !== 2) {
-				callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+	if (offset) {
+		callback(this.RESULT_ATTR_NOT_LONG);
+	} else if (data.length !== 2) {
+		callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+	} else {
+		if (offset) {
+			callback(this.RESULT_ATTR_NOT_LONG);
+		}
+		else if (data.length !== 1) {
+			callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
+		}
+		else {
+			let id = data.readUInt8(0);
+			if (verifyId(id)) {
+				confirmSpot(this.parkingSpot.location, id)
+				// LED.writeSync(0)
 			} else {
-				if (offset) {
-					callback(this.RESULT_ATTR_NOT_LONG);
-				}
-				else if (data.length !== 1) {
-					callback(this.RESULT_INVALID_ATTRIBUTE_LENGTH);
-				}
-				else {
-					let id = data.readUInt8(0);
-					if (verifyId(id)) {
-						confirmSpot(parkingSpot.spot, id)
-						// LED.writeSync(0)
-					} else {
-						// LED.writeSync(1)
-					}
-				}
+				// LED.writeSync(1)
 			}
 		}
+	}
+}
 module.exports = PermitAuthRequestCharacteristic
